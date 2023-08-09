e2e640358d0e910f6793d63992af3b1296528e89
ARCICO08 <124898373+ARCICO08@users.noreply.github.com>
2023-08-09 11:00:54 -0500

    node
    SENA

---
+++ nodejsproyect/src/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {dirname, join} from 'path'
+import {fileURLToPath} from "url"
+import { copyFile } from "fs";
+import indexRoutes from './routes/index.js'
+
+const app = express ()
+
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+app.set('views', join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(express.static(join(__dirname, 'public')))
+
+app.use(indexRoutes)
+
+
+
+app.listen(5000)
+console.log("server is listening on port ", 5000);
+
+
+
